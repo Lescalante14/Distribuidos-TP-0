40acecc4e25d98302c6be5a58a1f66e5736aed34
Luis Escalante <lescalante@fi.uba.ar>
2025-09-04 19:42:21 -0300

    only sleep to test 8

--- generar-compose.sh
+++ generar-compose.sh
@@ -2,6 +2,5 @@
 echo "Nombre del archivo de salida: $1"
 echo "Cantidad de clientes: $2"
 python3 generar-compose.py $1 $2
-# delete csv files in .data folder
-rm -f .data/agency-*.csv
+# for test 8
 sleep 2
